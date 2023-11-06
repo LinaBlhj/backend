39c1d0777c03f595b962d98c5f6df8386beecbda
LinaBlhj <lina.blhj@outlook.fr>
2023-11-06 17:39:50 +0100

    auth en commentaire dans routes

--- app/routes/stuff.js
+++ app/routes/stuff.js
@@ -6,9 +6,9 @@ const auth = require('../middleware/auth.js')
 const multer = require('../middleware/multer-config');
 const stuffCtrl = require('../controller/tutorial.controller.js');
 
-router.get('/', auth, stuffCtrl.findAll);
+router.get('/', /*auth,*/ stuffCtrl.findAll);
 
-router.post('/', auth, multer, stuffCtrl.create);
+router.post('/', /*auth, multer,*/ stuffCtrl.create);
 /*
 router.get('/:id', auth, stuffCtrl.findOne);
 
