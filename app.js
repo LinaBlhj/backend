f45efca8c5a07a5a3c4f05ce564b2aa4d546fb5c
LinaBlhj <lina.blhj@outlook.fr>
2023-12-05 09:48:37 +0100

    cors

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ mongoose.connect('mongodb+srv://lina:scaolO4MansRER32@cluster0.wsovhbg.mongodb.n
 );
 */
 const app = express();
-
+const cors = require('cors');
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use(express.json());
@@ -23,6 +23,10 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
+app.use(cors({
+  origin: '*',
+  methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
+}));
 
 app.use('/api/stuff', stuffRoutes);
 app.use('/api/auth', userRoutes);
